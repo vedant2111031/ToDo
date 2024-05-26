06d8cb66a95889663a1f328c3dd1b130409d6370
vedant pandey <pvedant69@gmail.com>
2024-05-26 12:57:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="container ">
+      <div className="bg-red-600">hey</div>
+     </div>
+    </>
+  )
+}
+
+export default App
